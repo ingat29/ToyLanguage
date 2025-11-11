5502d3c1c38d1e6a99e0ded459db691ac00566e7
ingat_29 <ignatdavid290@gmail.com>
2025-11-11 18:18:56 +0200

    Formatting

--- src/view/ExitCommand.java
+++ src/view/ExitCommand.java
@@ -4,6 +4,7 @@ public class ExitCommand extends Command {
     public ExitCommand(String key, String desc){
         super(key, desc);
     }
+
     @Override
     public void execute() {
         System.exit(0);
--- src/view/TextMenu.java
+++ src/view/TextMenu.java
@@ -7,14 +7,20 @@ import java.util.Scanner;
 public class TextMenu {
 
     private Map<String, Command> commands;
-    public TextMenu(){ commands=new HashMap<>(); }
+
+    public TextMenu(){
+        commands=new HashMap<>();
+    }
+
     public void addCommand(Command c){ commands.put(c.getKey(),c);}
+
     private void printMenu(){
         for(Command com : commands.values()){
             String line=String.format("%4s : %s", com.getKey(), com.getDescription());
             System.out.println(line);
         }
     }
+
     public void show(){
         Scanner scanner=new Scanner(System.in);
         while(true){
@@ -29,4 +35,3 @@ public class TextMenu {
         }
     }
 }
-
