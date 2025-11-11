9e625146b27bfa8b788e2dc3ae3a487170b85c64
ingat_29 <ignatdavid290@gmail.com>
2025-11-11 18:15:43 +0200

    Formatting

--- src/view/Interpreter.java
+++ src/view/Interpreter.java
@@ -31,7 +31,7 @@ class Interpreter {
         // Example 1: int v; v=2; Print(v)
         IStmt ex1 = new CompStmt(new VarDeclStmt("v", new IntType()),
                 new CompStmt(new AssignmentStmt("v", new ValueExpression(new IntValue(2))),
-                        new PrintStmt(new VariableExpression("v"))));
+                new PrintStmt(new VariableExpression("v"))));
 
         PrgState prg1 = new PrgState(new MyStack<>(), new MyDictionary<>(), new FileTable(), new MyList<>(), ex1);
         List<PrgState> list1 = new ArrayList<>();
@@ -42,9 +42,9 @@ class Interpreter {
         // Example 2: int a; int b; a=2+3*5; b=a+1; Print(b)
         IStmt ex2 = new CompStmt(new VarDeclStmt("a", new IntType()),
                 new CompStmt(new VarDeclStmt("b", new IntType()),
-                        new CompStmt(new AssignmentStmt("a", new ArithmeticalExpression(ArithmeticalOperation.ADD, new ValueExpression(new IntValue(2)), new ArithmeticalExpression(ArithmeticalOperation.MULTIPLY, new ValueExpression(new IntValue(3)), new ValueExpression(new IntValue(5))))),
-                                new CompStmt(new AssignmentStmt("b", new ArithmeticalExpression(ArithmeticalOperation.ADD, new VariableExpression("a"), new ValueExpression(new IntValue(1)))),
-                                        new PrintStmt(new VariableExpression("b"))))));
+                new CompStmt(new AssignmentStmt("a", new ArithmeticalExpression(ArithmeticalOperation.ADD, new ValueExpression(new IntValue(2)), new ArithmeticalExpression(ArithmeticalOperation.MULTIPLY, new ValueExpression(new IntValue(3)), new ValueExpression(new IntValue(5))))),
+                new CompStmt(new AssignmentStmt("b", new ArithmeticalExpression(ArithmeticalOperation.ADD, new VariableExpression("a"), new ValueExpression(new IntValue(1)))),
+                new PrintStmt(new VariableExpression("b"))))));
 
         PrgState prg2 = new PrgState(new MyStack<>(), new MyDictionary<>(), new FileTable(), new MyList<>(), ex2);
         List<PrgState> list2 = new ArrayList<>();
@@ -56,9 +56,9 @@ class Interpreter {
         // Example 3: bool a; int v; a=true; (If a Then v=2 Else v=3); Print(v)
         IStmt ex3 = new CompStmt(new VarDeclStmt("a", new BoolType()),
                 new CompStmt(new VarDeclStmt("v", new IntType()),
-                        new CompStmt(new AssignmentStmt("a", new ValueExpression(new BoolValue(true))),
-                                new CompStmt(new IfStmt(new VariableExpression("a"), new AssignmentStmt("v", new ValueExpression(new IntValue(2))), new AssignmentStmt("v", new ValueExpression(new IntValue(3)))),
-                                        new PrintStmt(new VariableExpression("v"))))));
+                new CompStmt(new AssignmentStmt("a", new ValueExpression(new BoolValue(true))),
+                new CompStmt(new IfStmt(new VariableExpression("a"), new AssignmentStmt("v", new ValueExpression(new IntValue(2))), new AssignmentStmt("v", new ValueExpression(new IntValue(3)))),
+                new PrintStmt(new VariableExpression("v"))))));
 
         PrgState prg3 = new PrgState(new MyStack<>(), new MyDictionary<>(), new FileTable(), new MyList<>(), ex3);
         List<PrgState> list3 = new ArrayList<>();
@@ -70,13 +70,13 @@ class Interpreter {
         // string varf; varf="test.in"; openRFile(varf); int varc; readFile(varf,varc); print(varc); readFile(varf,varc); print(varc); closeRFile(varf)
         IStmt ex4 = new CompStmt(new VarDeclStmt("varf", new StringType()),
                 new CompStmt(new AssignmentStmt("varf", new ValueExpression(new StringValue("test.in"))),
-                        new CompStmt(new OpenRFileStmt(new VariableExpression("varf")),
-                                new CompStmt(new VarDeclStmt("varc", new IntType()),
-                                        new CompStmt(new ReadFileStmt(new VariableExpression("varf"), "varc"),
-                                                new CompStmt(new PrintStmt(new VariableExpression("varc")),
-                                                        new CompStmt(new ReadFileStmt(new VariableExpression("varf"), "varc"),
-                                                                new CompStmt(new PrintStmt(new VariableExpression("varc")),
-                                                                        new CloseRFileStmt(new VariableExpression("varf"))))))))));
+                new CompStmt(new OpenRFileStmt(new VariableExpression("varf")),
+                new CompStmt(new VarDeclStmt("varc", new IntType()),
+                new CompStmt(new ReadFileStmt(new VariableExpression("varf"), "varc"),
+                new CompStmt(new PrintStmt(new VariableExpression("varc")),
+                new CompStmt(new ReadFileStmt(new VariableExpression("varf"), "varc"),
+                new CompStmt(new PrintStmt(new VariableExpression("varc")),
+                new CloseRFileStmt(new VariableExpression("varf"))))))))));
 
         PrgState prg4 = new PrgState(new MyStack<>(), new MyDictionary<>(), new FileTable(), new MyList<>(), ex4);
         List<PrgState> list4 = new ArrayList<>();
